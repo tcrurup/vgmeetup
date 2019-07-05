faae582602be681d8eb77dec6470daefd20b34b2
Anthony Rurup <tcrurup@gmail.com>
2019-07-05 22:39:34 +0000

    add GamesController and mount to config file

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,8 @@
+class GamesController < ApplicationController
+
+  get '/games/index' do
+    'success'
+    #erb :"games/index"
+  end
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+#ActiveRecord::Base.establish_connection(
+#  :adapter => "sqlite3",
+#  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+#)
 
 configure :development do
   set :database, "sqlite3:db/development.sqlite"
