a389fb875adaedc7bc2f5cf6f839170f28d9bf0c
Anthony Rurup <tcrurup@gmail.com>
2019-07-05 01:47:10 +0000

    add database seeds

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+user1 = {firstname: "tony", lastname: "rurup", email: "tony.rurup@gmail.com", password: 'tactics'}
+user2 = {firstname: "cody", lastname: "applen", email: "cody.applen@gmail.com", password: 'bingbong'}
+user3 = {firstname: "nazif", lastname: "khan", email: "nazif.khan@gmail.com", password: 'listen'}
+user4 = {firstname: "nathan", lastname: "legrand", email: "nathan.legrand@gmail.com", password: 'minime'}
+
+
+User.create(user1)
+User.create(user2)
+User.create(user3)
+User.create(user4)
