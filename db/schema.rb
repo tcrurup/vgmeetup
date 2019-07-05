0f3ed697e0fe8bbb18396f116037a0770f9a9c28
Anthony Rurup <tcrurup@gmail.com>
2019-07-05 01:38:21 +0000

    add models and views directory

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_07_04_184327) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
 end
