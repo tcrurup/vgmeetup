9b78faff6b721a6a86869e6a2f63867d82bd9e32
Anthony Rurup <tcrurup@gmail.com>
2019-07-05 22:49:45 +0000

    user can create games through games/new view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,16 @@
 class GamesController < ApplicationController
 
+  #-----Create-----
+  get '/games/new' do
+    erb :'games/new'
+  end
+
+  post '/games' do
+    Game.create(params[:game])
+  end
+
+  #-----Read-----
+
   get '/games/index' do
     'success'
     #erb :"games/index"
