852643b316ce416618c05229973f65226040bc92
Anthony Rurup <tcrurup@gmail.com>
2019-07-05 03:37:27 +0000

    adds styling and anchor to login page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,11 @@
 class UserController < ApplicationController
 
-  get '/login' do
+  get '/user/login' do
     erb :'users/login'
+  end
+
+  get '/user/sign_up' do
+    erb :'users/sign_up'
   end
 
 end
