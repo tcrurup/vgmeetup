91fbf58f384679b8119ea34da150b14b5fa401c1
Anthony Rurup <tcrurup@gmail.com>
2019-07-10 22:27:17 +0000

    user can submit new wall post through post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,8 +1,16 @@
 class PostController < ApplicationController
 
+  #-----Create-----
+
   get "/posts/new/:slug" do
     @recipient = User.find_by_slug(params[:slug])
-    erb "post/new"
+    erb :"post/new"
+  end
+
+  post "/posts/:slug" do
+    recipient = User.find_by_slug(params[:slug])
+    current_user.post_to_other_users_board(params[:post_content], recipient)
+    redirect "/user/#{recipient.slug}"
   end
 
 end
