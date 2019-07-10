467c6b76bee4842e8654ba3b36f3cc5030bb628d
Anthony Rurup <tcrurup@gmail.com>
2019-07-10 01:19:19 +0000

    include button to add game to user collection

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -17,4 +17,12 @@ class UserController < ApplicationController
     erb :'user/homepage'
   end
 
+  #-----Update-----
+
+  post '/user/add_game/:game_slug' do
+    game = Game.find_by_slug(params[:game_slug])
+    current_user.add_game_to_collection(game)
+    redirect "/games/#{game.slug}"
+  end
+
 end
