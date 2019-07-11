4f26363b3dfdc70e8fb07c8e9d39f9b1caa963e4
Anthony Rurup <tcrurup@gmail.com>
2019-07-11 19:04:20 +0000

    clicking on button in genre index will take you to list of similar games

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,15 @@
 class GenresController < ApplicationController
 
+  #-----Create-----
+
+
+  #-----Read-----
   get '/genres' do
     erb :'genres/index'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'genres/show'
   end
 end
