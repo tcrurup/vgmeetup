192cf23eb80b3ebe3c0de600644e5a72e50ed71c
Anthony Rurup <tcrurup@gmail.com>
2019-07-11 00:15:51 +0000

    add username method for friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,14 @@
 class Friend < ActiveRecord::Base
+
   has_many :user_friends
+  has_many :users, through: :user_friends
+
+  def friend
+    @friend ||= User.find(self.user_id)
+  end
+
+  def username
+    friend.username
+  end
+
 end
